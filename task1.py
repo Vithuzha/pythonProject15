9ca401722c12f32d4eb014259abc36ce5f5e3a89
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-01-23 14:42:33 +0530

    added

---
+++ task1.py
@@ -0,0 +1,9 @@
+a=int(input("any number"))
+if(a>20):
+    print("I likeIcecream")
+elif(a>30):
+    print("I Like Chocolate")
+elif(a>60):
+    print("I Like Apple")
+else:
+    print("I do not like anything")
