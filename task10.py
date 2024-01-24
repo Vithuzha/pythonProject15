222b61c72c3ed82eb1f0938fd139dbf51b8d884c
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-01-24 11:11:27 +0530

    added

---
+++ task10.py
@@ -0,0 +1,9 @@
+mark=int(input("enter any mark"))
+if((mark<=100)and(mark>=90)):
+    print("A")
+elif((mark<=89)and(mark>=80)):
+    print("B")
+elif((mark<=79)and(mark>=70)):
+    print("C")
+elif(mark<70):
+    print("D")
