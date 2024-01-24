74759c85dd2dc3e88e2fdebfe14e3279b54e8441
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-01-24 11:19:14 +0530

    added

---
+++ task11.py
@@ -0,0 +1,15 @@
+day=int(input("enter any number"))
+if day==1:
+    print(day,"-monday")
+elif day==2:
+    print(day,"-tuesday")
+elif day==3:
+    print(day,"-wednesday")
+elif day==4:
+    print(day,"-thursday")
+elif day==5:
+    print(day,"-friday")
+elif day==6:
+    print(day,"-saturday")
+elif day==7:
+    print(day,"-sunday")
