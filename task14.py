6e2c723804fc9ec241d7135e322cec9f33cafadc
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-01-24 11:49:03 +0530

    added

---
+++ task14.py
@@ -0,0 +1,7 @@
+speed=int(input("enter any speed"))
+if (speed>31 and speed<=40):
+    print("50 pounds")
+elif(speed>=41 and speed<=50):
+    print("75 pounds")
+elif(speed>50):
+    print("100 pounds")
