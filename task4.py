03079567bef6386eb61ce163c33e6c65ab38de58
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-01-26 08:21:32 +0530

    added

---
+++ task4.py
@@ -0,0 +1,8 @@
+ch = input("enter any character:")
+if ((ch>='a') and (ch<='z')):
+    print("it is an alphabet")
+elif ((ch>='A') and (ch<='Z')):
+    print("it is an alphabet")
+else:
+    print("it is not an alphabet")
+
