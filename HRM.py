5ff3a9430097a6c9dbe1a2df872e62917629a69a
https://github.com/Vithuzha/Programming.git <vithuvimal15@gmail.com>
2024-02-06 18:07:14 +0530

    added

---
+++ HRM.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self, emp_id, emp_name,emp_dep, emp_contact, emp_app, emp_status):
+        self.emp_id = emp_id
+        self.emp_name = emp_name
+        self.emp_dep = emp_dep
+        self.emp_contact = emp_contact
+        self.emp_app = emp_app
+        self.emp_status = emp_status
+    def add_employee(self, emp_id, emp_name, emp_dep, emp_contact, emp_app, emp_status):
+        pass
+
+    def view_emp_info(self):
+        pass
+
+    def update_emp_info(self):
+        pass
+
+    def leave_request(self):
+        pass
+
+    def view_company_policy(self):
+        pass
